5f9f3649d2976f3b84b73f02edc4e603437f5973
David Ramiro <david@kore.cc>
2019-05-21 02:39:27 +0200

    add support for different architectures

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # gpay-gms-patch
 Google Pay can be pretty hard to get to work with Magisk, with this simple shell script you can get it to work flawlessly on the more recent versions of GPay, Google Play Services and Magisk.
 
+I have included SQLite3 binaries from the official Android NDK, the script can automatically download the correct version from this repo, but feel free to use your own and place it into `/system/xbin` with its permissions set to 755.
+
 ## Tested on:
-- Working as of 17.05.2019:
+- Working as of 21.05.2019:
   - Xiaomi Mi9
   - Android 9, May Security Patch
   - Magisk 19.1
@@ -21,14 +23,15 @@ com.google.android.gms
 com.google.android.gsf
 ```
 * Obfuscate Magisk Manager package string in Magisk settings
-* Check if SafetyNet passes. If not:
-  * Spoof a legitimate device fingerprint (easy to do with MagiskHide Props Config module)
+* Check if SafetyNet passes. If ctsProfile fails:
+  * Spoof a legitimate device fingerprint (easy to do with [MagiskHide Props Config module](https://github.com/Magisk-Modules-Repo/MagiskHidePropsConf/blob/master/README.md#spoofing-devices-fingerprint-to-pass-the-ctsprofile-check))
 * Transfer `gpay-gms-patch.sh` to your device
 * Open your terminal emulator (or `adb shell`) and browse to the script's directory
 * Make the script executable with `chmod +x gpay-gms-patch.sh`
 * Get SU permissions by entering `su` (confirm the Magisk prompt)
 * Execute the script by entering `sh gpay-gms-patch.sh`
-* Reboot the device and go to a store to buy something.
+* Follow the on screen instructions.
+* Reboot the device and add your cards to Google Pay.
 
 ## Note on Updating
 Of course this might get patched in the future. You can always roll back Google Play Services and redo the process. I will try to keep this repo updated if anything changes.
--- gpay-gms-patch.sh
+++ gpay-gms-patch.sh
@@ -1,38 +1,105 @@
-#!/sbin/su
+#!/system/bin/sh
+
+echo ""
+
+# check for root
+if [ $(id -u) = 0 ]
+then
+  echo "We are root. Let's go!"
+else
+   echo "Script needs to be run as root. Did you use 'su'?"
+   exit 1
+fi
 
 # mount /system with write permissions
-echo "mounting /system with rw..."
+echo "Mounting /system with RW permissions..."
 mount -o remount,rw /system
+echo ""
+sleep 2
 
-# checking if sqlite3 is present
-if [ -f "/system/xbin/sqlite3" ]
+
+# checking if sqlite3 is missing
+if [ ! -f "/system/xbin/sqlite3" ]
 then
-	echo "sqlite3 binary is present. moving on."
+  echo "SQLite3 not found. Do you want to download it now? "
+  # prompt user if download is wanted
+	select yn in "Yes" "No"; do
+    case $yn in
+      Yes )
+        echo "Getting device architecture..."
+        abi=$(getprop ro.product.cpu.abi)
+        echo "Device architecture is $abi."
+        echo ""
+        sleep 1
+        echo "Downloading SQLite3 binary for $abi..."
+        cd /system/xbin/
+        # use device architecture in the download url
+        curl -O https://raw.githubusercontent.com/davidramiro/gpay-gms-patch/master/bin/"$abi"/sqlite3
+        sleep 2
+        # check if download worked before continuing
+        if [ -f "/system/xbin/sqlite3" ]
+        then
+          echo "Applying permissions..."
+          chmod 755 sqlite3
+          sleep 1
+          echo "SQLite3 installed in /system/xbin."
+          echo ""
+          sleep 1
+        else
+          echo "Download failed. Internet/permission issue?"
+          echo "Reverting /system mount and exiting..."
+          mount -o remount,ro /system
+          exit 1
+        fi
+        break
+        ;;
+      No )
+        echo ""
+        echo "This script needs an SQLite3 binary to be present in /system/xbin."
+        echo "Please download it manually and use chmod 755."
+        echo "Reverting /system mount and exiting..."
+        mount -o remount,ro /system
+        exit 1
+        ;;
+    esac
+  done
 else
-	echo "sqlite3 binary not found. downloading..."
-	cd /system/xbin/
-	curl -O https://raw.githubusercontent.com/davidramiro/gpay-gms-patch/master/bin/sqlite3
-	chmod 755 sqlite3
+  echo "SQLite3 binary is present. Moving on."
+  echo ""
+  sleep 2
 fi
 
 # force close gpay
-echo "stopping google pay..."
+echo "Stopping Google Pay..."
 am force-stop /data/data/com.google.android.apps.walletnfcrel
+echo ""
+sleep 2
 
 # if the file was already altered before, set its original permissions
-echo "setting default permissions..."
+echo "Setting default permissions on database..."
 chmod 660 /data/data/com.google.android.gms/databases/dg.db
+echo ""
+sleep 2
 
 # set 0 on every row containing "attest" on dg.db from GMS
-echo "editing database..."
+echo "Editing database..."
 /system/xbin/sqlite3 /data/data/com.google.android.gms/databases/dg.db "update main set c='0' where a like '%attest%';"
+echo ""
+sleep 2
 
 # make the file read-only
-echo "setting file read only..."
+echo "Setting database as read-only..."
 chmod 440 /data/data/com.google.android.gms/databases/dg.db
+echo ""
+sleep 2
 
 # finish by remounting /system as RO
-echo "remounting /system as read-only..."
+echo "Remounting /system as read-only..."
 mount -o remount,ro /system
+echo ""
+sleep 2
 
-echo "all done! reboot and enjoy google pay."
+echo "All done! Reboot, add your cards to Google Pay and have fun."
+echo "Big thanks to BostonDan@XDA!"
+echo ""
+exit 0
